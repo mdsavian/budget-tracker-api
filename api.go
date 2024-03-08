7d66a9065f54280d49d9e314e5d624bb660c30e9
mdsavian <marlon.savian@gmail.com>
2024-03-08 20:40:27 -0300

    feat(api): create writeJSON function, makeHTTPHandleFunc and uses it on the router handleGetAccount

--- api.go
+++ api.go
@@ -1,8 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type ApiError struct {
+	Error string
+}
+
+/*
+* Declared this type and the makeHttpHandleFunc function because we need to deal with the error
+* the core func HandlerFunc doesn't support the error so we need to convert it
+* to be able to use inside the mux
+ */
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := f(w, r)
+		if err != nil {
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
 
 type APIServer struct {
 	listenAddr string
@@ -15,7 +44,8 @@ func NewApiServer(listenAddr string) *APIServer {
 }
 
 func (s *APIServer) Start() {
-
+	router := mux.NewRouter()
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleGetAccount))
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, req *http.Request) error {
