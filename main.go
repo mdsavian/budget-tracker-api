b4df5ec5dc3f9c0197552b68561181277e729857
mdsavian <marlon.savian@gmail.com>
2024-06-11 10:06:04 -0300

    feat: execute import or server on main

--- main.go
+++ main.go
@@ -29,13 +29,15 @@ func main() {
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
+	if len(os.Args) > 2 {
+		importData := os.Args[1]
+		if ok, _ := strconv.ParseBool(importData); ok && os.Args[2] != "" {
+			path := os.Args[2]
+			cmd.ImportData(path, store)
 
-	server := apiserver.NewServer(fmt.Sprintf(":%s", portString), store)
-	server.Start()
-
-	importData := os.Args[1]
-	if ok, _ := strconv.ParseBool(importData); ok && os.Args[2] != "" {
-		path := os.Args[2]
-		cmd.ImportData(path)
+		}
+	} else {
+		server := apiserver.NewServer(fmt.Sprintf(":%s", portString), store)
+		server.Start()
 	}
 }
